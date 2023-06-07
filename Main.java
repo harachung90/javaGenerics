bb475e611edba03a8e6e600eee23d18f3e55195e
Hara <harachung90@gmail.com>
2023-06-07 21:35:24 +1000

    Second commit. Generic functions

--- Main.java
+++ Main.java
@@ -8,10 +8,15 @@ public class Main {
         Box<Milk> boxOfMilk = new Box();
         Box<Oranges> boxOfOranges = new Box();
 
-        boxOfMilk.add(milk);
-        boxOfOranges.add(oranges);
+        debugAdd(milk, boxOfMilk);
+        debugAdd(oranges, boxOfOranges);
 
         boxOfMilk.remove().drink();
         boxOfOranges.remove().juggle();
+    }
+
+    static <T> void debugAdd(T item, Box<T> box) {
+        System.out.println("Type: " + item.getClass().getCanonicalName());
+        box.add(item);
     }
 }
